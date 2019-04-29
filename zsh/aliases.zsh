fe8e22e2e7bc4fbc173f0659d984da7cd4fcef8b
Zach Colon <zach.colon@adwerx.com>
2019-04-29 11:39:02 -0400

    Add some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,16 @@
 alias tmux='TERM=screen-256color-bce tmux'
-#
+
+# Terraform Aliases
+alias tf='terraform'
+alias tf="terraform"
+alias tfa="terraform apply"
+alias tfd="terraform destroy"
+
+#Kube Aliases
+alias k='kubectl'
+
+# FZF
+alias gbf="git checkout \`git branch | fzf\`"
+
+# Docker
+alias dc="docker-compose"
